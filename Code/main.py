aa1ff7bec7bd22f4e6e4202959977ca13b2ef745
Julien Amblard <julien.amblard16@imperial.ac.uk>
2020-03-02 10:21:24 +0000

    Clean up Samples dir

--- Code/main.py
+++ Code/main.py
@@ -2,6 +2,10 @@ import argparse
 
 from text_to_summary import TextToSummary
 
+FORMAT = 'txt'
+POS_SUMMARIES_EXT = '_summaries_pos'
+NEG_SUMMARIES_EXT = '_summaries_neg'
+
 
 def process_args(args):
     pos_summaries = None
@@ -14,12 +18,19 @@ def process_args(args):
         return args.text, pos_summaries, neg_summaries
     if args.file:
         return open(args.file).read(), pos_summaries, neg_summaries
+    if args.all_files:
+        dir = '{}/{}'.format(args.all_files, args.all_files)
+        text = open('{}.{}'.format(dir, FORMAT)).read()
+        pos_summaries = open('{}{}.{}'.format(dir, POS_SUMMARIES_EXT, FORMAT)).read()
+        neg_summaries = open('{}{}.{}'.format(dir, NEG_SUMMARIES_EXT, FORMAT)).read()
+        return text, pos_summaries, neg_summaries
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     command_group = parser.add_mutually_exclusive_group(required=True)
     command_group.add_argument('-f', '--file', type=str, help='path to text file to summarise')
+    command_group.add_argument('-a', '--all_files', type=str, help='path to folder with text file and summaries')
     command_group.add_argument('-t', '--text', type=str, help='text to summarise (use double quotation marks)')
     parser.add_argument('-p', '--pos_summaries', type=str, help='path to file containing positive summaries')
     parser.add_argument('-n', '--neg_summaries', type=str, help='path to file containing negative summaries')
