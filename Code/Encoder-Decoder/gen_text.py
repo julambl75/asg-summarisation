20e858c2772e21a39a9f1cf9f0e6414b871fb9d7
Julien Amblard <julien.amblard16@imperial.ac.uk>
2020-04-02 10:16:15 -0700

    Add Rexamples to generate RNN training data

---
+++ Code/Encoder-Decoder/gen_text.py
@@ -0,0 +1,15 @@
+'The flowers are blooming now' # DT NNS V VING ADV
+'The leaves are growing'
+'Spring is coming very soon'
+-> 'Spring is coming'
+
+'The wall is covered in poison ivy' # DT NN V VING IN NN
+'At night the house is full of ghosts'
+'You can hear voices coming from inside'
+'The house must be haunted'
+-> 'At night the house is full of ghosts so it is haunted'
+
+'Suzanne went to cooking school'
+'Sophie learned to cook in Australia'
+'They are owners of a famous restaurant in Melbourne'
+-> 'Sophie and Suzanne are renowned chefs' # NNP CC NNP V ADJ NNS
