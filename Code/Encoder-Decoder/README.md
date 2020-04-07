582a278aa378a1d5ed739eebecce6fe0c7a78264
Julien Amblard <julien.amblard16@imperial.ac.uk>
2020-04-06 17:03:44 -0700

    Write story/summary example generation code

--- Code/Encoder-Decoder/README.md
+++ Code/Encoder-Decoder/README.md
@@ -1,3 +1,4 @@
 A very large majority of the neural network code in this directory is adapted from the following site: https://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html.
 
 The data in the words directory come from WordNet: https://wordnet.princeton.edu/download/current-version.
+The names come from NamesDatabse: https://github.com/smashew/NameDatabases/blob/master/NamesDatabases/first%20names/all.txt.
--- Code/Encoder-Decoder/gen_data/gen_text.py
+++ Code/Encoder-Decoder/gen_data/gen_text.py
@@ -30,64 +30,66 @@
 # We need a large number of pairs for train (~9,000), a small number for test (~1,000), and a few for eval (~10).
 ###
 import os
+import random
 
 from pattern.en import conjugate, singularize, pluralize, referenced, lemma
 from datamuse import datamuse
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
-WORD_TYPES = ['adj', 'adv', 'noun', 'verb', 'names']
+WORD_TYPES = ['adj', 'adv', 'noun', 'verb', 'name']
 
 
-def read_data(word_type):
-    wordbank = open(f'{PATH}/words/{word_type}.txt', encoding='utf-8').read().strip().split('\n')
-    return list(map(lambda w: w.replace('_', ' '), wordbank))
+class GenData:
+    def __init__(self):
+        self.words = {word_type: self.read_data(word_type) for word_type in WORD_TYPES}
+        self.datamuse_api = datamuse.Datamuse()
+        # There is a bug with Python 3.7 causing the first call to Pattern to crash due to a StopIteration
+        try:
+            lemma('eight')
+        except:
+            pass
+
+    @staticmethod
+    def read_data(word_type):
+        wordbank = open(f'{PATH}/words/{word_type}.txt', encoding='utf-8').read().strip().split('\n')
+        return list(map(lambda w: w.replace('_', ' '), wordbank))
+
+    def get_random_word(self, word_type):
+        return random.choice(self.words[word_type])
+
+    def find_common_adj_for_noun(self, noun):
+        words = self.datamuse_api.words(rel_jjb=noun, max=5)
+        return random.choice(words)['word'] if words else None
+
+    def find_synonym_with_context(self, noun, context):
+        words = self.datamuse_api.words(rel_syn=noun, topics=context, max=5)
+        return random.choice(words)['word'] if words else None
+
+    def make_summary(self, subject, descriptor, adjective):
+        other_descriptor = self.find_synonym_with_context(descriptor, adjective)
+        other_adjective = self.find_common_adj_for_noun(descriptor)
+        if other_descriptor is None or other_adjective is None:
+            return None
+        verb = conjugate('be', tense='past', person=3, number='singular')
+        s_object_ind = referenced(descriptor, article='indefinite')
+        s_object_def = referenced(other_descriptor, article='definite')
+        s_object_adj = referenced(' '.join([adjective, descriptor]), article='indefinite')
+        summary = ' '.join([subject, verb, s_object_adj])
+        sentence1 = ' '.join([subject, verb, s_object_ind])
+        sentence2 = ' '.join([s_object_def, verb, adjective, 'and', other_adjective])
+        return summary, [sentence1, sentence2]
 
 
-# >>> verb = "go"
-# >>> conjugate(verb,
-# ...     tense = "past",           # INFINITIVE, PRESENT, PAST, FUTURE
-# ...    person = 3,                # 1, 2, 3 or None
-# ...    number = "singular",       # SG, PL
-# ...      mood = "indicative",     # INDICATIVE, IMPERATIVE, CONDITIONAL, SUBJUNCTIVE
-# ...    aspect = "imperfective",   # IMPERFECTIVE, PERFECTIVE, PROGRESSIVE
-# ...   negated = False)            # True or False
-# u'went'
-
-# When using pattern, there is a bug with Python 3.7 causing the first call to result in a StopIteration error
-def initialize():
-    global datamuse_api
-    try:
-        lemma('eight')
-    except:
-        pass
-    datamuse_api = datamuse.Datamuse()
-
-
-# https://www.clips.uantwerpen.be/pages/pattern-en#conjugation
-def make_sentence(subject, verb, v_object, adjective=None, adverb=None):
-    np = referenced(subject, article='indefinite')
-    vp = conjugate(verb, tense='past', person=3, number='plural')
-    return ' '.join((np, vp, adjective, pluralize(v_object), adverb))
-
-
+# https://www.clips.uantwerpen.be/pages/pattern-en
 # https://www.datamuse.com/api/
-def make_summary(subject):
-    np = referenced(subject, article='definite')
-    vp = conjugate('be', tense='present', person=3, number='singular')
-    # datamuse_api.words(rel_)
-
-
 if __name__ == '__main__':
-    initialize()
+    gen_data = GenData()
 
-    words = {word_type: read_data(word_type) for word_type in WORD_TYPES}
-
-    for i in range(10, 20):
-        adjective = words['adj'][i]
-        adverb = words['adv'][i]
-        noun = words['noun'][i]
-        noun2 = words['noun'][i + 20]
-        verb = words['verb'][i]
-
-        print(make_sentence(noun, verb, noun2, adjective, adverb))
+    for i in range(5):
+        print(gen_data.make_summary('Joe', 'dog', 'rambunctious'))
+    for i in range(10):
+        subject = gen_data.get_random_word('name')
+        descriptor = gen_data.get_random_word('noun')
+        adjective = gen_data.get_random_word('adj')
+        print(gen_data.make_summary(subject, descriptor, adjective))
