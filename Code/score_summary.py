223f597a741ba1f869c1e7dff1e59a81e19ae843
Julien Amblard <julien.amblard16@imperial.ac.uk>
2020-04-13 15:29:24 -0700

    Finish scoring class

--- Code/score_summary.py
+++ Code/score_summary.py
@@ -2,29 +2,38 @@ import argparse
 import pprint as pp
 from collections import defaultdict
 
+import language_check
+
 from helper import Helper
 from parse_concept_net import ParseConceptNet
 
 SAME_WORD_SIMILARITY = 5
 SAME_WORD_POS = ['NN', 'NNS', 'NNP', 'NNPS']
 
+IGNORE_GRAMMAR_ERRORS = ['MORFOLOGIK_RULE_EN_GB']
+GRAMMAR_ERROR_PENALTY = 0.75
 
-class ScoreSummary:
+
+class SummaryScorer:
     def __init__(self, story, summary):
         self.story = story
         self.summary = summary
 
         self.pcn = ParseConceptNet(False)
         self.helper = Helper()
+        self.language_checker = language_check.LanguageTool('en-GB')
 
     def score(self):
-        score = self.similarity_score()
+        similarity_score = self.similarity_score()
 
         bleu_score = self.helper.bleu_score(story, summary)
-        bleu_coefficient = self.helper.count_sentences(story) / self.helper.count_sentences(summary)
-        print(bleu_score, bleu_coefficient)
+        bleu_score *= self.helper.count_sentences(story) / self.helper.count_sentences(summary)
 
-        return score
+        grammar_penalty = self.grammar_penalty()
+
+        length_penalty = self.length_penalty()
+
+        return similarity_score * bleu_score * grammar_penalty * length_penalty
 
     def similarity_score(self):
         tokenized_story = self.helper.tokenize_text(self.story, ignore_sentence=True)
@@ -35,23 +44,31 @@ class ScoreSummary:
 
         for story_word, story_pos in tokenized_story:
             for summary_word, summary_pos in tokenized_summary:
-                if story_pos == summary_pos and story_word == summary_word:
-                    similarity = SAME_WORD_SIMILARITY if story_pos in SAME_WORD_POS else 0
-                else:
-                    similarity = self.pcn.compare_words(story_word, summary_word)
-                    summary_similarity += similarity
-                if similarity > 0:
-                    similar_words[story_word][summary_word] = similarity
+                if story_pos == summary_pos:
+                    if story_word == summary_word:
+                        similarity = SAME_WORD_SIMILARITY if story_pos in SAME_WORD_POS else 0
+                    else:
+                        similarity = self.pcn.compare_words(story_word, summary_word)
+                        summary_similarity += similarity
+                    if similarity > 0:
+                        similar_words[story_word][summary_word] = similarity
         pp.pprint({k: dict(v) for k, v in dict(similar_words).items()})
         return summary_similarity
 
-    # def grammar_score(self):
-    #     import grammar_check
-    #     tool = grammar_check.LanguageTool('en-GB')
-    #     text = 'This are bad.'
-    #     matches = tool.check(text)
-    #     len(matches)
-    #     grammar_check.correct(text, matches)
+    # Decrease final score by 25% for every grammar error other than uncommon proper noun
+    def grammar_penalty(self):
+        errors = self.language_checker.check(self.summary)
+        num_errors = len(list(filter(lambda m: m.ruleId not in IGNORE_GRAMMAR_ERRORS, errors)))
+        return GRAMMAR_ERROR_PENALTY ** num_errors
+
+    # Divide final score by 2 for ever sentence over limit in summary
+    def length_penalty(self):
+        story_length = self.helper.count_sentences(self.story)
+        if story_length > 3:
+            penalty = abs(self.helper.count_sentences(self.summary) - 3)
+        else:
+            penalty = abs(self.helper.count_sentences(self.summary) - story_length + 1)
+        return 1 / (2 ** penalty)
 
 
 def parse_args():
@@ -64,5 +81,6 @@ def parse_args():
 
 if __name__ == '__main__':
     story, summary = parse_args()
-    score_summary = ScoreSummary(story, summary)
-    print(score_summary.score())
+    summary_scorer = SummaryScorer(story, summary)
+    score = summary_scorer.score()
+    print(score)
