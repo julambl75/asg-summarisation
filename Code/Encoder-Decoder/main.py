7c99d38f33c6d46ab4ba3240b0802dfdaebaf9a6
Julien Amblard <julien.amblard16@imperial.ac.uk>
2020-04-16 20:20:12 -0700

    Change hyper-parameters

--- Code/Encoder-Decoder/main.py
+++ Code/Encoder-Decoder/main.py
@@ -8,7 +8,6 @@ from train import Trainer
 from utils import DEVICE
 
 HIDDEN_SIZE = 128
-NUM_LAYERS = 3
 
 if __name__ == '__main__':
     lang = Lang()
@@ -19,8 +18,8 @@ if __name__ == '__main__':
 
     lang.prepare_embeddings()
 
-    encoder = EncoderRNN(lang.n_words, HIDDEN_SIZE, num_layers=NUM_LAYERS).to(DEVICE)
-    attn_decoder = AttnDecoderRNN(lang.n_words, HIDDEN_SIZE, seq_length, dropout_p=0.1, num_layers=NUM_LAYERS).to(DEVICE)
+    encoder = EncoderRNN(lang.n_words, HIDDEN_SIZE).to(DEVICE)
+    attn_decoder = AttnDecoderRNN(lang.n_words, HIDDEN_SIZE, seq_length, dropout_p=0.2).to(DEVICE)
 
     trainer = Trainer(lang, encoder, attn_decoder, train_pairs, seq_length)
     trainer.train_iters(30000, print_every=500)
