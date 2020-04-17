639cb43fca549765d7bea957b6cfa1c20481d1c3
Julien Amblard <julien.amblard16@imperial.ac.uk>
2020-04-17 15:33:04 -0700

    Try bidirectional LSTM

--- Code/Encoder-Decoder/model_rnn.py
+++ Code/Encoder-Decoder/model_rnn.py
@@ -8,22 +8,21 @@ from utils import DEVICE
 
 
 class EncoderRNN(nn.Module):
-    def __init__(self, embedding_size, hidden_size, num_layers=1):
+    def __init__(self, embedding_size, hidden_size, bidirectional=False):
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
-        self.num_layers = num_layers
 
         self.embedding = nn.Embedding(embedding_size, self.hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size, num_layers)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, bidirectional=bidirectional)
 
     def forward(self, input, hidden):
         embedded = self.embedding(input).view(1, 1, -1)
         output = embedded
-        output, hidden = self.gru(output, hidden)
+        output, hidden = self.lstm(output, hidden)
         return output, hidden
 
     def init_hidden(self):
-        return torch.zeros(self.num_layers, 1, self.hidden_size, device=DEVICE)
+        return torch.zeros(1 + int(self.bidirectional), 1, self.hidden_size, device=DEVICE)
 
 
 class AttnDecoderRNN(nn.Module):
@@ -37,7 +36,7 @@ class AttnDecoderRNN(nn.Module):
         self.attn = nn.Linear(self.hidden_size * 2, seq_length)
         self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
         self.dropout = nn.Dropout(self.dropout_p)
-        self.gru = nn.GRU(self.hidden_size, self.hidden_size, num_layers)
+        self.lstm = nn.LSTM(self.hidden_size, self.hidden_size, num_layers)
         self.out = nn.Linear(self.hidden_size, embedding_size)
 
     def forward(self, input, hidden, encoder_outputs):
@@ -51,7 +50,7 @@ class AttnDecoderRNN(nn.Module):
         output = self.attn_combine(output).unsqueeze(0)
 
         output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
+        output, hidden = self.lstm(output, hidden)
 
         output = F.log_softmax(self.out(output[0]), dim=1)
         return output, hidden, attn_weights
